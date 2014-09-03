472bd6da9a08eca99d386a6286ea614527f266b5
CarlosEscobar <cai_escobar@hotmail.com>
2014-09-03 00:23:22 -0600

    Terminando Entrega1

--- Prediliga.Presentation/Scripts/controllers.js
+++ Prediliga.Presentation/Scripts/controllers.js
@@ -397,8 +397,6 @@ angular.module('app.controllers', [])
 
         //  console.log($rootScope.rootligs[0].nombre);
 
-        $scope.predict = false;
-
         $scope.lospartidos = [{ idjuego: 123, equipo1: 'Barcelona', equipo2: 'Real Madrid', s1:0, s2:0, fecha: new Date(), predicted:0 },
                               { idjuego: 34, equipo1: 'Juventus', equipo2: 'Milan', s1: 0, s2: 0, fecha: new Date(), predicted: 0 },
                               { idjuego: 728, equipo1: 'Olimpia', equipo2: 'Real España', s1: 0, s2: 0, fecha: new Date(), predicted: 0 },
@@ -411,9 +409,40 @@ angular.module('app.controllers', [])
             $location.path(path);
         };
 
-        $scope.predi = function() {
-            $scope.predict = true;
+        $scope.isPredicting = false;
+
+        $scope.Equipo1 = "";
+        $scope.Score1 = "";
+        $scope.Equipo2 = "";
+        $scope.Score2 = "";
+
+        $scope.StartPrediction = function(team1, team2) {
+            $scope.isPredicting = true;
+            $scope.Equipo1 = team1;
+            $scope.Equipo2 = team2;
         };
+
+        $scope.cancelPredict = function() {
+            $scope.isPredicting = false;
+        };
+
+        $scope.Predict = function() {
+
+            for (var i = 0; i < $scope.lospartidos.length; i++) {
+                if (($scope.lospartidos[i].equipo1 === $scope.Equipo1) && ($scope.lospartidos[i].equipo2 === $scope.Equipo2)) {
+                    $scope.lospartidos[i].s1 = $scope.Score1;
+                    $scope.lospartidos[i].s2 = $scope.Score2;
+                    $scope.lospartidos[i].predicted = 1;
+                }
+            }
+
+            $scope.Score1 = "";
+            $scope.Score2 = "";
+
+            $scope.isPredicting = false;
+
+        };
+
     }])
 
 
